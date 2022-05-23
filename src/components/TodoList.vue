10d713c860927828e50088c323e356ab0c3862e4
Bence <gutanbence@gmail.com>
2022-05-23 09:56:08 +0300

    TDL-2 Position on page/media query

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    sajtoskiflimeaztszeretem
-  </div>
+  <ul class="flex flex-col gap-y-12">
+    <li class="border-2 border-black rounded-full">Card</li>
+    <li class="border-2 border-black rounded-full">Card</li>
+    <li class="border-2 border-black rounded-full">Card</li>
+  </ul>
 </template>
 
 <script>
 export default {
-  name: 'TodoList',
-  props: {}
-}
+  name: "TodoList",
+  props: {},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      desktop: "1024px",
+      // => @media (min-width: 1024px) { ...}
+    },
     extend: {
       colors: {
         green: {
